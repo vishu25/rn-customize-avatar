52103df8fbd832abd53462a67fbc06c14fbcdb6d
Vishu <apple@Vishu.local>
2023-03-01 18:36:14 +0530

    Add Imports index

---
+++ src/Avatar/avataaars/index.d.ts
@@ -0,0 +1,6 @@
+import AdventureNeutral from '../adventurerNeutral/components/AdventureNeutral';
+import {getAccessoriesSvg, createOwnAvatar, AvatarsTypes, OPTIONS, getClothingGraphicsSvg} from '../avataaars/utils/utils';
+
+export {getAccessoriesSvg, createOwnAvatar, OPTIONS, AvatarsTypes, getClothingGraphicsSvg};
+
+export default AdventureNeutral;
--- src/Avatar/index.js
+++
@@ -1,6 +0,0 @@
-import * as avataaars from './avataaars/components/Avataars';
-import * as adventurerNeutral from './adventurerNeutral/components/AdventureNeutral';
-
-// export {default as AdventureNeutral} from './adventurerNeutral/components/AdventureNeutral';
-
-export {adventurerNeutral, avataaars};
